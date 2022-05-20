98e9a6e373bc5a2a6c4d0dd669fd36adba8a2e38
ernest1298 <97247914+ernest1298@users.noreply.github.com>
2022-05-19 22:22:24 -0600

    hhjhj

---
+++ js/bom.js
@@ -0,0 +1,23 @@
+var arr = [];
+function bom() {
+
+  var variable = document.getElementById('favchap').value;
+  arr.push(variable);
+  document.getElementById('favchap').value = ""; 
+
+//   for (var i = 0; i < arr.length; i++) {
+//     // document.getElementById("list").innerHTML = arr[i];
+    console.log(arr)
+//   }
+
+document.getElementById("list").innerHTML = arr.join("\n")
+
+
+
+
+  
+
+
+    
+
+}
