bf2867951c835368e159ea1a9e47121f2559213b
ernest1298 <97247914+ernest1298@users.noreply.github.com>
2022-05-20 01:27:46 -0600

    bb

--- js/bom.js
+++ js/bom.js
@@ -1,27 +1,17 @@
-
+var arr = [];
 function bom() {
-  var arr = [];
+  
+  var final_display = "";
   var variable = document.getElementById('favchap').value;
   arr.push(variable);
   document.getElementById('favchap').value = ""; 
-
-//   for (var i = 0; i < arr.length; i++) {
-//     // document.getElementById("list").innerHTML = arr[i];
-    console.log(arr)
-//   }
-var display = ""; 
-for (var i = 0; i < arr.length; i++){
-    display += `<p>${arr[i]} <button class="delete">X</button><p>`; 
-    document.getElementById("list").innerHTML = display; 
-}
+var display = []; 
  
-// document.getElementById("list").innerHTML = display;
+for (var i = 0; i < arr.length; i++){
+    display[i] = `<div><p>${arr[i]} <button class="delete">X</button></p></div>`; 
+};
 
+final_display=display.join('');
 
-
-  
-
-
-    
-
+document.getElementById("list").innerHTML = final_display;
 }
