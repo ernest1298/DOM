320095b0f25d0d27f38a23a14525d954d8adf0d5
ernest1298 <97247914+ernest1298@users.noreply.github.com>
2022-05-20 11:37:26 -0600

    nn

--- js/bom.js
+++ js/bom.js
@@ -1,17 +1,45 @@
 var arr = [];
+var display = [];
+var final_display = "";
 function bom() {
   
   var final_display = "";
   var variable = document.getElementById('favchap').value;
   arr.push(variable);
   document.getElementById('favchap').value = ""; 
-var display = []; 
+ 
  
 for (var i = 0; i < arr.length; i++){
-    display[i] = `<div><p>${arr[i]} <button class="delete">X</button></p></div>`; 
+    display[i] = `<div><p>${arr[i]} <button onclick="del()" class="delete" id = "out${i}">X</button></p></div>`; 
 };
+
+
 
 final_display=display.join('');
 
 document.getElementById("list").innerHTML = final_display;
+
+}
+
+function del() {
+    check = true;
+    var i=0;
+     while(check){
+        if (event.target.id == "out"+i){
+            check = false;
+        }
+        else{
+        i++;}
+     }
+
+     index = i;
+     display.splice(index,1);
+     console.log(index);
+     final_display=display.join('');
+     document.getElementById("list").innerHTML = final_display;
+
+
+     
+
+  
 }
